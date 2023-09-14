374398b26b2f2bd8f1a89d91881d69370d893a69
Petru99Ch <117940325+Petru99Ch@users.noreply.github.com>
2023-09-14 14:59:21 +0300

    Add files via upload

--- Shop-Final/src/Cart/Cart.js
+++ Shop-Final/src/Cart/Cart.js
@@ -10,7 +10,7 @@ class Cart {
     
     
     
-    addToCart(productID, quantity = 1){   
+    addToCart(productID,srcImage, title ,quantity = 1){   
         let arr
 
         if(localStorage.getItem('product') != null){
@@ -18,6 +18,8 @@ class Cart {
 
              arr.push({
                 itemsID: productID,
+                srcImage: srcImage,
+                titleItemms: title,
                 quantity: quantity
             })
         }
@@ -27,6 +29,8 @@ class Cart {
         this.items.push(
             {
                 itemsID: productID,
+                srcImage: srcImage,
+                titleItemms: title,
                 quantity: quantity
             }
         )
@@ -39,11 +43,85 @@ class Cart {
         }
 
         
-
         let nominal = document.querySelector(".nominal")
-        nominal.innerText = this.items.length
-        
-
-
+        nominal.innerText = this.items.length  
+            
     }
+
+    renderCart(imageSrc, producTitle, quantity = 1) {
+        
+        let showCart = document.createElement('div')
+        showCart.classList.add('list-group','list-custom','list-position')
+        let nav = document.querySelector("body > div > nav")
+        nav.appendChild(showCart)
+    
+        let cart = document.querySelector("body > div > nav > div")
+
+        
+        let itemsCart = document.createElement('a')
+        itemsCart.href="#"
+        itemsCart.classList.add('list-group-item','list-custom') 
+
+
+
+        let imageItem = document.createElement('img')       
+            imageItem.src = imageSrc
+    
+        let titleItems = document.createElement('h2')
+        titleItems.innerText = producTitle
+
+        let quantityItems = document.createElement('p')
+        quantityItems.innerText = "X "+ quantity
+
+
+
+        itemsCart.appendChild(imageItem)
+        itemsCart.appendChild(titleItems)
+        itemsCart.appendChild(quantityItems)
+
+        cart.appendChild(itemsCart)
+          
+       
+        
+        
+          
+     }
+
+     saveCart(quantity=1){
+        let showCart = document.createElement('div')
+        showCart.classList.add('list-group','list-custom','list-position')
+        let nav = document.querySelector("body > div > nav")
+        nav.appendChild(showCart)
+        let cart = document.querySelector("body > div > nav > div")
+
+        let productStorage = JSON.parse(localStorage.getItem('product'))
+
+        
+            productStorage.forEach(product => {
+                let itemsCart = document.createElement('a')
+                itemsCart.href="#"
+                itemsCart.classList.add('list-group-item','list-custom') 
+    
+    
+    
+                let imageItem = document.createElement('img')       
+                    imageItem.src = product.srcImage
+            
+                let titleItems = document.createElement('h2')
+                titleItems.innerText = product.titleItemms
+    
+                let quantityItems = document.createElement('p')
+                quantityItems.innerText = "X "+ quantity
+    
+    
+    
+                itemsCart.appendChild(imageItem)
+                itemsCart.appendChild(titleItems)
+                itemsCart.appendChild(quantityItems)
+    
+                cart.appendChild(itemsCart)
+           
+       
+            })
+     }
 }
